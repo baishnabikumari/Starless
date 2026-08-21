924a3317287c4428a18b8618ecf1d672d74fbed8
baishnabikumari <baishnabi.5.06.2011@gmail.com>
2026-08-21 23:35:46 +0530

    feat: add GMST and local sidereal time calculation( now getGMST that is geenwich mean sidereal time and getLST that is the longitude shifts to the local meridian(360deg / 24hr = 15 deg /h yeah).

--- astronomy.js
+++ astronomy.js
@@ -1,3 +1,19 @@
 function getJulianDate(date){
     return date.getTime() / 86400000 + 2440587.5;
 }
+
+function getGMST(jd){
+    const daysSinceJ2000 = jd - 2451545.0;
+    let gmst = 18.697374558 + 24.06570982441908 * daysSinceJ2000;
+    gmst = gmst % 24;
+    if(gmst < 0) gmst += 24;
+    return gmst;
+}
+
+function getLST(jd, longitude){
+    const gmst = getGMST(jd);
+    let lst = gmst + longitude / 15;
+    lst = lst % 24;
+    if(lst < 0) lst += 24;
+    return lst;
+}
